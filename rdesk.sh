99038a03a525527e979dcaaf39178cd1b91c93ac
ericklopper <erictklopper@gmail.com>
2016-04-20 13:19:30 +0200

    Update rdesk.sh

--- rdesk.sh
+++ rdesk.sh
@@ -7,7 +7,13 @@ CONN=ConnNameOrIP
 
 read -p 'Please enter your user name: ' USER
 read -p 'Please enter the domain: ' DOMAIN
-read -p 'Please enter the password: ' PASS
+
+stty_orig=`stty -g` stty -echo
+#read secret
+read -p 'Please enter the password: ' secret
+stty $stty_orig
+
+
 read -p 'Please enter the IP or Server name to connect too: ' CONN
 
 echo 'Thank you, connecting to $CONN!'
