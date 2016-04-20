830a124e7cd62d2c4fd831096c5872fe2f8b7fc6
ericklopper <erictklopper@gmail.com>
2016-04-20 11:37:54 +0200

    Update rdesk.sh

--- rdesk.sh
+++ rdesk.sh
@@ -10,6 +10,6 @@ read -p 'Please enter the domain: ' DOMAIN
 read -p 'Please enter the password: ' PASS
 read -p 'Please enter the IP or Server name to connect too: ' CONN
 
-echo 'Thank you, connecting to RDP!'
+echo 'Thank you, connecting to $CONN!'
 
 rdesktop -u $USER -p $PASS -d $DOMAIN -f $CONN &
